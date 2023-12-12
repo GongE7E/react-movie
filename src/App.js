90e0d0fe30702b4a9cf30bcacb7f74a7e1e19666
gong <gong4737@icloud.com>
2023-12-12 20:48:35 +0900

    feat: Add pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Outlet } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+const queryClient = new QueryClient();
 function App() {
-  return <div className='App'>start</div>;
+  return (
+    <div className='App'>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Outlet />
+      </QueryClientProvider>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,40 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import Movies from './pages/Movies';
+import Home from './pages/Home';
+import Tvs from './pages/Tvs';
+import Watches from './pages/Watches';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/movie/:id',
+        element: <Movies />,
+      },
+      {
+        path: '/tv-show/:id',
+        element: <Tvs />,
+      },
+      { path: '/watch/:id', element: <Watches /> },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
