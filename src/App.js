7976c6aa5e2a2e16687e4267b69cdb4c40b8fa6a
gong <gong4737@icloud.com>
2023-12-13 20:51:31 +0900

    feat:Add useEffect data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import { Outlet } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import Header from './components/Header';
+import { useEffect, useState } from 'react';
 
 const queryClient = new QueryClient();
+
 function App() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:3000/data/movieData.json')
+      .then((res) => res.json())
+      .then((data) => setMovies(data));
+  }, []);
+
   return (
     <div className='App'>
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialIsOpen={false} />
-        <Outlet />
+        <Header />
+        <Outlet context={{ movies }} />
       </QueryClientProvider>
     </div>
   );
